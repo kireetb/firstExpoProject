be110b512dcbef060ff80ad9c8d57b630eeafd32
kireetb <bhat.kireet@gmail.com>
2023-06-15 09:42:47 -0400

    First successful build

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-native-paper'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import { theme } from './src/core/theme'
-import { StartScreen } from './src/screens/StartScreen'
+import StartScreen from './src/screens/StartScreen'
 
 const Stack = createStackNavigator()
 
